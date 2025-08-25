6ac1c05fdd14d156b7b71e38119e3863df372d65
Angêlica Araújo <134542537+araujo-angel@users.noreply.github.com>
2025-08-25 11:26:16 -0300

    Update NotificadorEmail.java questoes de seguranca

--- src/notifier/NotificadorEmail.java
+++ src/notifier/NotificadorEmail.java
@@ -21,7 +21,7 @@ public class NotificadorEmail implements NotificadorObserver {
 
     private void enviarEmail(String destinatario, String assunto, String corpo, String caminhoAnexo) {
         final String remetente = "anandaguedesdoo@gmail.com";
-        final String senha = "kthdipwxqmubotkv"; 
+        final String senha = ""; 
 
         System.out.println("📤 Preparando email...");
         System.out.println("Remetente: " + remetente);
